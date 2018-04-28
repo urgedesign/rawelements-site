61cd7064045764c4ae19a0f2fec82a17173adaa7
Glen Honeybone <glen@urgedesign.co.nz>
2018-04-29 11:05:47 +1200

    Design tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,15 @@ import logo from '../img/logo.svg'
 const IndexPage = () => (
   <div className="content">
     <img src={logo} className="logo" />
-    <h1>Handmade Custom Furniture</h1>
-    <p>Nick Gordon</p>
+    <h1 className="title">Handmade Custom Furniture</h1>
+    <p>
+      Nick Gordon<br />
+      <span className="descr">Designer &amp; Maker</span>
+    </p>
     <p>
       P. <a href="tel:0212270060">021 227 0060</a>
-      <br />
+    </p>
+    <p>
       E. <a href="mailto:nick@rawelements.co.nz">nick@rawelements.co.nz</a>
     </p>
   </div>
